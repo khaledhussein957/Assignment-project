ce3e6ea4af20d5361b5884d0c6e44a6ba8482db0
khaled hussein <khaledhussein957@gmail.com>
2025-12-24 15:35:51 +0300

    Update the app

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -4,42 +4,40 @@ import DashboardPage from "./pages/DashboardPage.tsx";
 import ProductsPage from "./pages/ProductsPage.tsx";
 import OrdersPage from "./pages/OrdersPage.tsx";
 import DashboardLayout from "./layouts/DashboardLayout";
-import { authClient } from "./lib/auth-client";
+// import { authClient } from "./lib/auth-client";
 
 function App() {
-  const { data: session, isPending } = authClient.useSession();
+  // const { data: session, isPending } = authClient.useSession();
 
-  if (isPending) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-base-200">
-        <span className="loading loading-spinner loading-lg text-primary"></span>
-      </div>
-    );
-  }
+  // if (isPending) {
+  //   return (
+  //     <div className="min-h-screen flex items-center justify-center bg-base-200">
+  //       <span className="loading loading-spinner loading-lg text-primary"></span>
+  //     </div>
+  //   );
+  // }
 
-  const isSignedIn = !!session;
+  // const isSignedIn = !!session;
 
   return (
     <Routes>
-      <Route
+      {/* <Route
         path="/login"
         element={isSignedIn ? <Navigate to={"/dashboard"} /> : <LoginPage />}
-      />
+      /> */}
 
-      <Route
+      {/* <Route
         path="/"
         element={isSignedIn ? <DashboardLayout /> : <Navigate to={"/login"} />}
-      >
+      > */}
+      <Route path="/" element={<DashboardLayout />}>
         <Route index element={<Navigate to={"dashboard"} />} />
         <Route path="dashboard" element={<DashboardPage />} />
         <Route path="products" element={<ProductsPage />} />
         <Route path="orders" element={<OrdersPage />} />
       </Route>
 
-      <Route
-        path="*"
-        element={<Navigate to={isSignedIn ? "/dashboard" : "/login"} />}
-      />
+      <Route path="*" element={<Navigate to={"/dashboard"} />} />
     </Routes>
   );
 }
